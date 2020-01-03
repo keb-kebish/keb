52dcd842370992eb3e64a51e64a27fd808670031
Michal Horčičko <horcicko.michal@gmail.com>
2020-01-03 22:54:07 +0100

    code cleanup

--- keb-core/build.gradle.kts
+++ keb-core/build.gradle.kts
@@ -10,20 +10,18 @@ repositories {
 }
 
 dependencies {
-    testImplementation(project(":keb-junit5"))
-
     api(group = "org.seleniumhq.selenium", name = "selenium-java", version = "3.141.59")
+
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation(kotlin("stdlib-jdk8"))
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation(project(":keb-junit5"))
     testImplementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "3.7.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
     testImplementation("org.assertj:assertj-core:3.11.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
-
-    testImplementation(group = "org.springframework", name = "spring-core", version = "5.2.2.RELEASE")
     testImplementation(group = "io.vertx", name = "vertx-web", version = "3.8.4")
 
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
 }
 
 tasks {
