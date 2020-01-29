1ef840fe521b86a493fe6d39129fc1173d1c1bfb
Michal Horčičko <horcicko.michal@gmail.com>
2020-01-29 13:18:55 +0100

    required content tests

--- keb-core/src/test/kotlin/org/kebish/core/ContentTest.kt
+++ keb-core/src/test/kotlin/org/kebish/core/ContentTest.kt
@@ -1,7 +1,9 @@
 package org.kebish.core
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
 import org.junit.jupiter.api.Test
+import org.openqa.selenium.NoSuchElementException
 
 class ContentTest {
 
@@ -59,6 +61,30 @@ class ContentTest {
 
         // then -
         assertThat(callCounter).isEqualTo(1)
+    }
+
+    @Test
+    fun `for required content is thrown an exception when empty content provided`() {
+        // given
+        val empty by content(required = true) { EmptyWebElement("") }
+
+        // when
+        val result = catchThrowable { empty }
+
+        // then
+        assertThat(result).isInstanceOf(NoSuchElementException::class.java)
+    }
+
+    @Test
+    fun `for not-required content exception is not thrown when empty content provided`() {
+        // given
+        val empty by content(required = false) { EmptyWebElement("") }
+
+        // when
+        val result = empty
+
+        // then
+        assertThat(result).isInstanceOf(EmptyWebElement::class.java)
     }
 
 }
