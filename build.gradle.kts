131e8bea65a2ce2b2b025eb251990ecadbe6cb23
Jan Vondrouš <vondrous@gmail.com>
2020-02-01 22:38:03 +0100

    Additional info for bintray publishing code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 
 allprojects {
     group = "org.kebish"
-    version = "0.2.1"
+    version = "0.2.2"
+    description = "Library for browser tests implementing Page Object pattern and using Selenium"
 
     repositories {
         mavenLocal()
--- keb-core/build.gradle.kts
+++ keb-core/build.gradle.kts
@@ -1,16 +1,17 @@
 import com.jfrog.bintray.gradle.BintrayExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import java.util.*
 
 fun findProperty(s: String) = project.findProperty(s) as String?
 
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.3.61" //TODO  Versions class
 
     // Publish plugins - start
     `java-library`
     `maven-publish`
-    id("com.jfrog.bintray") version "1.8.4"
+    id("com.jfrog.bintray") version "1.8.4" //TODO  Versions class
     // Publish plugins - end
 }
 
@@ -22,13 +23,20 @@ bintray {
     pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
         repo = "kebish"
         name = "keb-core"
-//        userOrg = "bintray_user"
+//        userOrg = "Kebish"
         setLicenses("WTFPL")
         vcsUrl = "https://gitlab.com/horca23/keb.git"
+        websiteUrl = "https://gitlab.com/horca23/keb"
+        description = "Library for browser tests implementing Page Object pattern and using Selenium"
+        desc = description
+        setLabels("kotlin", "selenium", "pageobject")
 
-//        version(delegateClosureOf<BintrayExtension.VersionConfig> {
-//            name = "0.2"
-//        })
+        version(delegateClosureOf<BintrayExtension.VersionConfig> {
+            name = project.version.toString()
+            desc = "Kebish - Kotlin Page Object pattern implementation"
+            released = Date().toString()
+            vcsTag = project.version.toString()
+        })
         setPublications("keb-publish-artifact")
     })
 
@@ -89,3 +97,8 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
 }
 
+val testTask by tasks.registering {
+    doLast {
+        println("TEST TASKKKKKKKKKK '${project.version.toString()}'")
+    }
+}
