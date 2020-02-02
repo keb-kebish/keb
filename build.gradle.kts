e953087a9d216db2d0614048e153f6d05c112eca
Jan Vondrouš <vondrous@gmail.com>
2020-02-02 22:04:17 +0100

    Prepared next version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 allprojects {
     group = "org.kebish"
-    version = "0.2.3"
+    version = "0.2.5-SNAPSHOT"
     description = "Library for browser tests implementing Page Object pattern and using Selenium"
 
     repositories {
