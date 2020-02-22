a81ab2c0911022065850264f3bc4a3a8da86c28b
horca <horcicko.michal@gmail.com>
2020-02-22 22:01:35 +0100

    checkbox module

---
+++ keb-core/src/main/kotlin/org/kebish/core/module/Checkbox.kt
@@ -0,0 +1,19 @@
+package org.kebish.core.module
+
+import org.openqa.selenium.WebElement
+
+class Checkbox(scope: WebElement) : FormElement(scope) {
+
+    override fun getInputType() = "checkbox"
+
+    fun isChecked() = formElement.isSelected
+
+    fun check() {
+        if (!isChecked()) formElement.click()
+    }
+
+    fun uncheck() {
+        if (isChecked()) formElement.click()
+    }
+
+}
