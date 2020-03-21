68383c67c0f00e1eb414e11ff9ab7bfdbf0a4400
Jan Vondrouš <vondrous@gmail.com>
2020-03-21 21:54:39 +0100

    Test - QuitBrowserInTheMiddleOfTheTest

---
+++ keb-core/src/test/kotlin/org/kebish/core/browser/QuitBrowserInTheMiddleOfTheTest.kt
@@ -0,0 +1,39 @@
+package org.kebish.core.browser
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.then
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.kebish.core.kebConfig
+import org.kebish.junit5.KebTest
+import org.mockito.Mockito.RETURNS_DEEP_STUBS
+
+class QuitBrowserInTheMiddleOfTheTest : KebTest(
+    kebConfig {
+        driver = { mock(defaultAnswer = RETURNS_DEEP_STUBS) }
+        browserManagement.apply {
+            closeBrowserAfterEachTest = true
+        }
+    }
+
+) {
+
+    @Test
+    fun `quit browser in the middle of the test - create new driver`() {
+        // Given
+        val driver1 = browser.driver
+
+        // When
+        browser.quit()
+
+        // Then - assert driver quit was called
+        then(driver1).should().quit()
+
+        // When - get browser after quit
+        val driver2 = browser.driver
+
+        // Then - new driver is returned
+        assertThat(driver2).isNotSameAs(driver1)
+    }
+
+}
