2f6488cd9e116e719cd646ad704aece3a9d666fb
Jan Vondrouš <vondrous@gmail.com>
2020-04-01 21:39:15 +0200

    Gradle plugin for search dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    // add task "dependencyUpdates" - which writes possible updates of all dependencies
+    id("com.github.ben-manes.versions") version "0.28.0"
+}
 
 allprojects {
     group = "org.kebish"
