c60cfbbb0b91d669460fe74cba58288835a8bb52
Jan Vondrouš <vondrous@gmail.com>
2020-09-09 07:20:19 +0200

    Gradle build files renamed according to module name.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,16 @@ rootProject.name = "keb"
 include("keb-core")
 include("keb-junit5")
 include("keb-bobril")
+
+
+fun setupBuildFileName(project: ProjectDescriptor) {
+    project.children.forEach {
+        it.apply {
+            buildFileName = "$name.gradle.kts"
+            assert(projectDir.isDirectory)
+            assert(buildFile.isFile)
+            setupBuildFileName(this)
+        }
+    }
+}
+setupBuildFileName(rootProject)
