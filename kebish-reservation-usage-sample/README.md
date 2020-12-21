89c992e09179f6d1d8d3ae002dc12bb402080955
Jan Vondrouš <vondrous@gmail.com>
2020-12-21 10:46:30 +0100

    Reservation app basic login admin:pass hello world rest controller static index html page readme
    #5

---
+++ kebish-reservation-usage-sample/README.md
@@ -0,0 +1,20 @@
+# Reservation application
+
+This is web application for creating "reservations".  
+Is's purpose is to demonstrate usage of "Kebish".
+
+## How to run
+`gradlew booRun`
+
+
+
+## Dev Notes
+
+user:pass = admin:pass  
+stored in `application.properties`
+
+
+Hello World Rest  
+http://localhost:8080/hello?name=Vondr
+
+
--- kebish-reservation-usage-sample/src/main/kotlin/org/kebish/sample/reservation/KebishReservationUsageSampleApplication.kt
+++ kebish-reservation-usage-sample/src/main/kotlin/org/kebish/sample/reservation/KebishReservationUsageSampleApplication.kt
@@ -2,10 +2,20 @@ package org.kebish.sample.reservation
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class KebishReservationUsageSampleApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 fun main(args: Array<String>) {
-	runApplication<KebishReservationUsageSampleApplication>(*args)
+    runApplication<KebishReservationUsageSampleApplication>(*args)
+}
+
+@SpringBootApplication
+@RestController
+class KebishReservationUsageSampleApplication {
+
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String): String {
+        return String.format("Hello %s!", name)
+    }
 }
