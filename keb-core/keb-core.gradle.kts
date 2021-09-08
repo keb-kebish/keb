0493e4ed439db4af72bf8eed41f5372bd16fd0de
Jan Vondrouš <vondrous@gmail.com>
2021-09-08 11:19:25 +0200

    Bump webdrivermanager

--- keb-core/keb-core.gradle.kts
+++ keb-core/keb-core.gradle.kts
@@ -1,10 +1,3 @@
-
-import com.jfrog.bintray.gradle.BintrayExtension
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-import org.gradle.api.tasks.testing.logging.TestLogEvent.*
-import org.kebish.Bintray
-import java.util.*
-
 plugins {
     id("kebbuild.keb-published-kotlin-module")
 }
@@ -13,7 +6,7 @@ dependencies {
     api(group = "org.seleniumhq.selenium", name = "selenium-java", version = "3.141.59")
 
     testImplementation(project(":keb-junit5"))
-    testImplementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "4.2.0")
+    testImplementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "5.0.1")
     testImplementation(group = "io.ktor", name = "ktor-server-netty", version = "1.4.0")
     testImplementation(group = "io.ktor", name = "ktor-html-builder", version = "1.4.0")
 }
