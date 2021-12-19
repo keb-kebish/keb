97fed3bc039f9534c1fa4e5c692584b63d4daf70
Jan Vondrouš <vondrous@gmail.com>
2021-12-19 10:27:53 +0100

    Commented out - jcenter publish plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
+//    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
 }
