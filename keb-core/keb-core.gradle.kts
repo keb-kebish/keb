d24f8244fab354fbdb77f47225c6eb2ed89013f1
Jan Vondrouš <vondrous@gmail.com>
2022-05-30 15:00:53 +0200

    Selenium update

--- keb-core/keb-core.gradle.kts
+++ keb-core/keb-core.gradle.kts
@@ -3,10 +3,10 @@ plugins {
 }
 
 dependencies {
-    api(group = "org.seleniumhq.selenium", name = "selenium-java", version = "3.141.59")
+    api(group = "org.seleniumhq.selenium", name = "selenium-java", version = "4.2.0")
 
     testImplementation(project(":keb-junit5"))
-    testImplementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "5.0.1")
+    testImplementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "5.2.0")
     testImplementation(group = "io.ktor", name = "ktor-server-netty", version = "1.5.4")
     testImplementation(group = "io.ktor", name = "ktor-html-builder", version = "1.5.4")
 }
