084f57904a1f5aecb6178f15284e2891881c6eb7
Jan Vondrouš <vondrous@gmail.com>
2022-05-30 14:41:13 +0200

    Minor dependency update. Transitive dependencies to "org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2" is missing.

--- keb-core/keb-core.gradle.kts
+++ keb-core/keb-core.gradle.kts
@@ -7,6 +7,6 @@ dependencies {
 
     testImplementation(project(":keb-junit5"))
     testImplementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "5.0.1")
-    testImplementation(group = "io.ktor", name = "ktor-server-netty", version = "1.4.0")
-    testImplementation(group = "io.ktor", name = "ktor-html-builder", version = "1.4.0")
+    testImplementation(group = "io.ktor", name = "ktor-server-netty", version = "1.5.4")
+    testImplementation(group = "io.ktor", name = "ktor-html-builder", version = "1.5.4")
 }
