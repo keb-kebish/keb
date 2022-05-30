ede5776ed251948a4eb0835b0079c77ebf71eb9f
Jan Vondrouš <vondrous@gmail.com>
2022-05-30 14:28:29 +0200

    Removed obsolete comments - bintray

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,4 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-//    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
 }
--- buildSrc/src/main/kotlin/org/kebish/Versions.kt
+++ buildSrc/src/main/kotlin/org/kebish/Versions.kt
@@ -2,5 +2,4 @@ package org.kebish
 
 object Versions {
 //    const val kotlin = "1.4.10"
-//    const val bintray = "1.8.4"
 }
