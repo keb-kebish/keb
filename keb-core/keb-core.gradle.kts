bed6d326bc9e018e6afff6f5ca4ebb787f513da8
Jan Vondrou? <bugsmail@seznam.cz>
2023-02-02 22:16:11 +0100

    Update of Selenium and webdrivermanager

--- keb-core/keb-core.gradle.kts
+++ keb-core/keb-core.gradle.kts
@@ -3,10 +3,10 @@ plugins {
 }
 
 dependencies {
-    api(group = "org.seleniumhq.selenium", name = "selenium-java", version = "4.2.0")
+    api(group = "org.seleniumhq.selenium", name = "selenium-java", version = "4.8.0")
 
     testImplementation(project(":keb-junit5"))
-    testImplementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "5.2.0")
+    testImplementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "5.3.2")
     testImplementation(group = "io.ktor", name = "ktor-server-netty", version = "1.6.8")
     testImplementation(group = "io.ktor", name = "ktor-html-builder", version = "1.6.8")
 }
